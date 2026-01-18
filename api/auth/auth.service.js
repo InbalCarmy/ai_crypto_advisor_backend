742b19e1ff7b16513133aa1e59f28e19673cd7e5
Inbal Carmy <inbal950@gmail.com>
2026-01-18 13:27:35 +0200

    fix dependencies and working with mongo db

---
+++ api/auth/auth.service.js
@@ -0,0 +1,62 @@
+import Cryptr from 'cryptr'
+import bcrypt from 'bcrypt'
+
+import { userService } from '../user/user.service.js'
+import { loggerService } from '../../services/logger.service.js'
+
+const cryptr = new Cryptr(process.env.SECRET1)
+
+export const authService = {
+    getLoginToken,
+    validateToken,
+    login,
+    signup
+}
+
+
+function getLoginToken(user) {
+    const str = JSON.stringify(user)
+    const encryptedStr = cryptr.encrypt(str)
+    return encryptedStr
+}
+
+function validateToken(token) {
+    try {
+        const json = cryptr.decrypt(token)
+        const loggedinUser = JSON.parse(json)
+        return loggedinUser
+    } catch (err) {
+        console.log('Invalid login token')
+    }
+    return null
+}
+
+async function login(email, password) {
+    var user = await userService.getByEmail(email)
+    if (!user) throw 'Unkown Email'
+
+    //  un-comment for real login
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) throw 'Invalid email or password'
+
+    // Removing passwords and personal data
+        const miniUser = {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        }
+    return miniUser
+}
+
+async function signup({ email, name, password }) {
+    const saltRounds = 10
+
+    loggerService.debug(`auth.service - signup with name: ${name}, email: ${email}`)
+    if (!name || !password || !email) throw 'Missing required signup information'
+
+    const userExist = await userService.getByEmail(email)
+    if (userExist) throw 'This email already taken'
+
+    const hash = await bcrypt.hash(password, saltRounds)
+    return userService.add({ email, password: hash, name })
+}
